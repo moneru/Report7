db0c7e109b9b7981ddea1264b898026b981d2083
Ryoma <e165743@ie.u-ryukyu.ac.jp>
2017-02-02 22:55:26 +0900

    2nd

---
+++ src/main/java/jp/ac/uryukyu/ie/e165743/TimerTest1.java
@@ -0,0 +1,46 @@
+package jp.ac.uryukyu.ie.e165743;
+
+import javax.swing.*;
+import java.awt.BorderLayout;
+import java.awt.event.*;
+
+public class TimerTest1 extends JFrame implements ActionListener{
+
+    Timer timer;
+    JLabel label;
+    int sec;
+
+    public static void main(String[] args){
+        TimerTest1 frame = new TimerTest1();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setBounds(10, 10, 300, 200);
+        frame.setTitle("タイトル");
+        frame.setVisible(true);
+    }
+
+    TimerTest1(){
+        sec = 1;
+        label = new JLabel();
+
+        JPanel labelPanel = new JPanel();
+        labelPanel.add(label);
+
+        timer = new Timer(1000 , this);
+
+        getContentPane().add(labelPanel, BorderLayout.CENTER);
+
+        timer.start();
+    }
+
+    public void actionPerformed(ActionEvent e){
+        label.setText(12-sec + " sec");
+
+        if (sec >= 11){
+            timer.stop();
+        }else{
+            sec++;
+        }
+
+    }
+}
--- src/main/java/jp/ac/uryukyu/ie/e165743/main.java
+++ src/main/java/jp/ac/uryukyu/ie/e165743/main.java
@@ -1,31 +1,65 @@
 package jp.ac.uryukyu.ie.e165743;
+import javax.swing.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.Container;
 import java.awt.BorderLayout;
+import java.awt.event.*;
 
-class main extends JFrame{
-    public static void main(String args []){
+class main extends JFrame implements ActionListener {
+    JLabel label;
+    Timer timer;
+    int sec;
+
+    public static void main(String args[]) {
         main Barrage = new main("title");
         Barrage.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Barrage.setVisible(true);
 
     }
 
-    main(String title){
+    main(String title) {
 
         setTitle(title);
         JPanel p = new JPanel();
-        setSize(400,500);
+        setSize(300, 300);
         setLocationRelativeTo(null);
 
+        label = new JLabel("");
 
         JButton button1 = new JButton("Lets Barrage!");
+        button1.addActionListener(this);
 
+        p.add(label);
         p.add(button1);
-        Container contentPane = getContentPane();
-        contentPane.add(p, BorderLayout.CENTER);
 
+        getContentPane().add(p);
+    }
+
+    public void Timer(){
+        sec = 1;
+        label = new JLabel("");
+
+        JPanel labelPanel = new JPanel();
+        labelPanel.add(label);
+
+        timer = new Timer(1000 , this);
+
+        getContentPane().add(labelPanel, BorderLayout.CENTER);
+
+        timer.start();
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        label.setText("Start!!");
+
+        label.setText(12-sec + " sec");
+
+        if (sec >= 11){
+            timer.stop();
+        }else {
+            sec++;
+        }
     }
 }
